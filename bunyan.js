db450132aa8ea36b6f930fa8bef50c121e759ccf
lizhiyuan <148334733@qq.com>
2020-09-22 14:18:09 +0800

    没有支持pm2的模块，所以无法正常使用....

---
+++ bunyan.js
@@ -0,0 +1,4 @@
+var bunyan = require('bunyan');
+var log = bunyan.createLogger({name:"myapp"})
+log.info("hi");
+log.warn({lang:"fr"},'au revoir');
--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     apps:[{
         name:"app",
-        script:"./app.js",
+        script:"./log4js.js",
         env:{
             NODE_ENV:"development"
         },
