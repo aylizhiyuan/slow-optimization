373df205a357a269945fe376703d785c8a9953fd
lizhiyuan <148334733@qq.com>
2020-09-25 18:34:50 +0800

    开始搞定定时任务的监控....

--- node守护进程/app.js
+++ node守护进程/app.js
@@ -1,10 +1,12 @@
 var http = require("http");
 
 http.createServer(function(req, res) {
-
+    if(req.url == '/test'){
+        resd.end("error");
+    }
     res.writeHead(200, {
         "Content-Type": "text/plain"
     });
+    
     res.end("Hello World");
-
 }).listen(8080);
--- node守护进程/ctl.js
+++ node守护进程/ctl.js
@@ -1,19 +1,15 @@
-var daemon = require("daemonize2").setup({
-    main: "app.js",
-    name: "sampleapp",
-    pidfile: "sampleapp.pid"
+// 为了支持监控子进程的运行状态和让子进程独立运行,我们使用spawn
+
+const spawn = require('child_process').spawn;
+const subprocess = spawn('node',['app.js'],{
+    detached:true,
+    stdio:'ignore' // pipe只能通过父进程输出 
 });
+subprocess.unref();
+// 成为守护进程的条件
+// 1. 子进程必须和父进程分离 ,及detached= true
+// 2. 默认情况下父进程会等待已分离的子进程,调用subprocess.unref()取消等待
+// 3. 子进程的IO和父进程的IO不能联系 stdio:ignore
 
-switch (process.argv[2]) {
 
-    case "start":
-        daemon.start();
-        break;
 
-    case "stop":
-        daemon.stop();
-        break;
-
-    default:
-        console.log("Usage: [start|stop]");
-}
