c49dd2237ebcc11322cd10af80543330c842916e
lizhiyuan <148334733@qq.com>
2020-10-19 15:11:31 +0800

    安装crc校验流..

--- node流stream/through2.js
+++ node流stream/through2.js
@@ -15,5 +15,72 @@
 
 // 实现源码
 
-const { Transform } = require('readable-stream');
+const { Transform } = require('readable-stream');// node核心的流实现
+function inherits(fn,sup){
+    fn.super_ = sup;
+    fn.prototype = Object.create(sup.prototype,{
+        constructor: { value: fn, enumerable: false, writable: true, configurable: true }
+    })
+}
+function through2(construct){
+    // 怪异的写法,只是为了重新定义through2函数,返回一个新的函数
+    // 检查参数的正确性，如正确则直接调用through2里的函数
+    // 函数会返回一个transform流
+    return (options,transform,flush) => {
+        if(typeof options === 'function'){
+            flush = transform
+            transform = options
+            options = {}
+        }
+        if(typeof transform !== 'function'){
+            transform = (chunk,enc,cb) => cb(null,chunk)
+        }
+        if(typeof flush !== 'function'){
+            flush = null;
+        }
+        return construct(options,transform,flush);
+    }
+}
+const make = through2((options,transform,flush)=>{
+    const t2 = new Transform(options);
+    t2._transform = transform;
+    if(flush){
+        t2._flush = flush;
+    }
+    return t2;
+})
+
+const obj = through2(function (options, transform, flush) {
+    const t2 = new Transform(Object.assign({ objectMode: true, highWaterMark: 16 }, options))
+    t2._transform = transform
+    if (flush) {
+      t2._flush = flush
+    }  
+    return t2
+})
+
+const ctor = through2((options, transform, flush) => {
+    function Through2 (override) {
+      if (!(this instanceof Through2)) {
+        return new Through2(override)
+      }
+  
+      this.options = Object.assign({}, options, override)
+  
+      Transform.call(this, this.options)
+  
+      this._transform = transform
+      if (flush) {
+        this._flush = flush
+      }
+    }
+  
+    inherits(Through2, Transform)
+  
+    return Through2
+})
+
+module.exports = make
+module.exports.ctor = ctor
+module.exports.obj = obj
 
