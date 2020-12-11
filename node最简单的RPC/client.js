7d81a81da9790824bd73f09c365761ce9351900b
lizhiyuan <148334733@qq.com>
2020-12-11 14:05:14 +0800

    正式开始整理微服务相关....其余不重要

---
+++ node最简单的RPC/client.js
@@ -0,0 +1,6 @@
+/*
+ * @Author: lizhiyuan
+ * @Date: 2020-12-11 14:04:26
+ * @LastEditors: lizhiyuan
+ * @LastEditTime: 2020-12-11 14:04:26
+ */
---
+++ node最简单的RPC/index.js
@@ -0,0 +1,6 @@
+/*
+ * @Author: lizhiyuan
+ * @Date: 2020-12-11 14:04:30
+ * @LastEditors: lizhiyuan
+ * @LastEditTime: 2020-12-11 14:04:30
+ */
